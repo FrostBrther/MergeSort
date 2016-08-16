00cf56e7149553cf6f7dd0b404090fa1814e5fbe
Frost_Brother <marsel-anarbaev@mail.ru>
2016-08-17 01:02:29 +0300

    Was added new function that merge divided arrays

--- Merge_Sort/Merge_Sort/main.cpp
+++ Merge_Sort/Merge_Sort/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void mergeArrays(int *arr);
 int main()
 {
 
 	return 0;
+}
+
+void mergeArrays(int *arr)
+{
+	//this function will merge arrays after sort them by dividing
 }
