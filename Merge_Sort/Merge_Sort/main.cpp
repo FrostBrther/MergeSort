45bf35962fad3fe89dd8b695f1d00f17d690a0d4
Frost_Brother <marsel-anarbaev@mail.ru>
2016-08-17 01:12:34 +0300

    Now function takes two arrays to megre them

--- Merge_Sort/Merge_Sort/main.cpp
+++ Merge_Sort/Merge_Sort/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void mergeArrays(int *arr);
+void mergeArrays(int *first_array,int *second_array);
 int main()
 {
 
 	return 0;
 }
 
-void mergeArrays(int *arr)
+void mergeArrays(int *first_array, int *second_array)
 {
 	//this function will merge arrays after sort them by dividing
 }
