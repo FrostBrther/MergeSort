0e369ee4296bc1918d17c3c8ae4759635fcc50f8
Frost_Brother <marsel-anarbaev@mail.ru>
2016-08-17 13:47:36 +0300

    added new function that divide array to two subarrays

--- Merge_Sort/Merge_Sort/main.cpp
+++ Merge_Sort/Merge_Sort/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void mergeArrays(int *first_array, int *second_array);
+void divide(int *array);
 int main()
 {
-
+	
 	return 0;
 }
 
 void mergeArrays(int *first_array, int *second_array)
 {
 	//this function will merge arrays after sort them by dividing
+}
+
+void divide(int *array)
+{
+	//this function will wivide array to two subarrays
 }
